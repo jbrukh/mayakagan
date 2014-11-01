bf8cde9081988b64bbc8fee51caf9f4df6460267
Jake Brukhman <jbrukh@gmail.com>
2014-11-01 15:34:56 -0400

    Seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'jbrukh@gmail.com', password: 'mayaissexy')
+User.create(email: 'mayakagan.design@gmail.com', password: 'jakeissexy')

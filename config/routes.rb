17fb9f6d365643fe9d493809c40ed128bc3f96bf
Jake Brukhman <jbrukh@gmail.com>
2014-11-01 15:20:19 -0400

    Admin routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+
+  ## Root
+
   root 'pages#home'
 
+  ## Admin
+
   namespace :admin do
+    devise_for :users, :skip => [:sessions]
+    as :user do
+      get 'login' => 'devise/sessions#new', :as => :new_user_session
+      post 'login' => 'devise/sessions#create', :as => :user_session
+      delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
     resources :projects
   end
 

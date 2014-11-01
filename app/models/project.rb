5b94f01339ae23be6ce9cc83c53cade75823d032
Jake Brukhman <jbrukh@gmail.com>
2014-11-01 18:16:53 -0400

    Added hero.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,13 @@
 class Project < ActiveRecord::Base
 
+  ## Thumbnail
+
   has_attached_file :thumbnail, :styles => { :thumb => "300x300>" }, :default_url => "missing.png"
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
+
+  ## Hero
+
+  has_attached_file :hero, :default_url => "missing.png"
+  validates_attachment_content_type :hero, :content_type => /\Aimage\/.*\Z/
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101205131) do
+ActiveRecord::Schema.define(version: 20141101221611) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20141101205131) do
     t.string   "thumbnail_content_type"
     t.integer  "thumbnail_file_size"
     t.datetime "thumbnail_updated_at"
+    t.string   "hero_file_name"
+    t.string   "hero_content_type"
+    t.integer  "hero_file_size"
+    t.datetime "hero_updated_at"
   end
 
   create_table "thumbnails", force: true do |t|

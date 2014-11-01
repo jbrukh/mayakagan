615036d5edd9eb70ba60829f5e810e26daedabc9
Jake Brukhman <jbrukh@gmail.com>
2014-11-01 16:43:52 -0400

    Added paperclip attributes.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+
+  has_attached_file :thumbnail, :styles => { :thumb => "300x300>" }, :default_url => "/assets/missing.png"
+  validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
+
 end

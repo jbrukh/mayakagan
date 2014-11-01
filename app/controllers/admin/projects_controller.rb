ae9f61711311620c2a06f05abf61ccea0d60823a
Jake Brukhman <jbrukh@gmail.com>
2014-11-01 16:41:10 -0400

    Home.

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProjectsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_project, :only => [:show, :edit, :update]
+  before_action :set_project, :only => [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -18,12 +18,25 @@ class Admin::ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new(permitted_params)
+    if @project.save
+      redirect_to admin_project_path(@project)
+    else
+      redirect_to admin_project_path(@project), alert: 'Could not save this project.'
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to admin_projects_path
   end
 
   def update
+    if @project.update(permitted_params)
+      redirect_to admin_project_path(@project)
+    else
+      redirect_to admin_project_path(@project), alert: 'Could not update this project.'
+    end
   end
 
   private
@@ -35,5 +48,5 @@ class Admin::ProjectsController < ApplicationController
     def permitted_params
       params.require(:project).permit(:title, :description)
     end
-    
+
 end

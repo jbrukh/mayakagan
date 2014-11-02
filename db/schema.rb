9c84947db66c2a3cda8cb0ba81a54338e79f5032
Jake Brukhman <jbrukh@gmail.com>
2014-11-02 00:33:35 -0400

    Added position column.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102040605) do
+ActiveRecord::Schema.define(version: 20141102043317) do
 
   create_table "project_details", force: true do |t|
     t.string   "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141102040605) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "position"
   end
 
   create_table "projects", force: true do |t|

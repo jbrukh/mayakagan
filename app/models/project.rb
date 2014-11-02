a8de20a24ef1320c50dbd0db79986bc621f5d5ca
Jake Brukhman <jbrukh@gmail.com>
2014-11-01 21:07:33 -0400

    Detail pages.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,12 +4,12 @@ class Project < ActiveRecord::Base
 
   ## Thumbnail
 
-  has_attached_file :thumbnail, :styles => { :thumb => "300x300>" }, :default_url => "missing.png"
+  has_attached_file :thumbnail, :styles => { :thumb => "350x350>" }, :default_url => "missing.png"
   validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
 
   ## Hero
 
-  has_attached_file :hero, :styles => { :thumb => "300x300>" }, :default_url => "missing.png"
+  has_attached_file :hero, :styles => { :thumb => "350x350>" }, :default_url => "missing.png"
   validates_attachment_content_type :hero, :content_type => /\Aimage\/.*\Z/
 
 end
--- app/models/project_detail.rb
+++ app/models/project_detail.rb
@@ -3,8 +3,8 @@ class ProjectDetail < ActiveRecord::Base
   belongs_to :project
 
   # Image
-  
-  has_attached_file :image, :default_url => "missing.png"
+
+  has_attached_file :image, :styles => { :thumb => "350x350>" }, :default_url => "missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end

055691d230aeaf1488f52b6d92639f04bd3508b8
Jake Brukhman <jbrukh@gmail.com>
2014-12-25 15:51:13 -0500

    Save.

--- db/migrate/20141225205055_add_centered_to_project.rb
+++ db/migrate/20141225205055_add_centered_to_project.rb
@@ -1,5 +1,5 @@
 class AddCenteredToProject < ActiveRecord::Migration
   def change
-    add_column :projects, :centered, :boolean
+    add_column :projects, :centered, :boolean, default: false
   end
 end
